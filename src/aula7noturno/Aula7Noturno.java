ff2ee39abe8c9c6c6a1c81172f35be40c0d078d0
killbert <>
2019-10-30 22:07:08 -0300

    aula7 criei senha

--- src/aula7noturno/Aula7Noturno.java
+++ src/aula7noturno/Aula7Noturno.java
@@ -20,10 +20,23 @@ public class Aula7Noturno {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
        Usuario user = new Usuario();
-        System.out.println("Digite seu nome:");
-       
+       user.senha = "1234";
+      String contraSenha;
         
+        System.out.println("Digite seu nome:");
              user.nome(sc.nextLine());  
+        System.out.println("Digite seu email:");
+            user.email(sc.nextLine());
+        System.out.println("Digite sua senha:");
+            user.senha(sc.nextLine());
+            System.out.println("Digite a senha novamente");
+            contraSenha = sc.nextLine();
+     
+            if(user.senha .equals(contraSenha)){
+                System.out.println("Voçê está logado");
+            }else{
+              user.contraSenha();
+            }
        
        
        
